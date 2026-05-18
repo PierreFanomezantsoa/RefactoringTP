47393af3d9a975a7dd3fb42cea8843744624a649
PierreFanomezantsoa <rnandrasanarivopierre@gmail.com>
2026-05-18 12:28:45 +0300

    Test de build automatique sur jenkins

--- src/main/java/iut/bad/consommation.java
+++ src/main/java/iut/bad/consommation.java
@@ -1,9 +1,9 @@
 package iut.bad;
-// Interface de consommation, à implémenter par les classes qui consomment de la nourriture et des boissons
+
 public interface consommation {
- // Méthode pour manger, à implémenter par les classes qui consomment de la nourriture
+
 	void manger();
- // Méthode pour boire, à implémenter par les classes qui consomment des boissons
+
 	void boire();
 
 }
